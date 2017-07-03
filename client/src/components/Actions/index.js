61e19f81ad648a105ccbf18e6320c84493b57ac3
ghaash <daniel.silverman@me.com>
2017-07-03 10:56:38 -0700

    finalized redux

--- client/src/components/Actions/index.js
+++ client/src/components/Actions/index.js
@@ -1,49 +1,37 @@
-// import axios from "axios";
+import uuid from 'uuid';
 
-export const FETCH_POSTS = "fetch_posts";
-export const FETCH_POST = "fetch_post";
+const actions = {
+    AddBook: (title, description, image_url) => {
+        return {
+            type: 'ADD_BOOK',
+            id: uuid.v4(),
+            title, 
+            description, 
+            image_url
+        };
+    }
+};
+
 export const CREATE_POST = "create_post";
-export const DELETE_POST = "delete_post";
 
-// const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-// const API_KEY = "?key=PAPERCLIP1234";
-
-export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
-  return {
-    type: FETCH_POSTS,
-    // payload: request
-  };
-}
-
-export function createPost(values, callback) {
-  const request = axios
-    .post(`${ROOT_URL}/posts${API_KEY}`, values)
-    .then(() => callback());
-
-  return {
-    type: CREATE_POST,
-    payload: request
-  };
-}
-
-export function fetchPost(id) {
-  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
-
-  return {
-    type: FETCH_POST,
-    payload: request
-  };
-}
-
-export function deletePost(id, callback) {
-  const request = axios
-    .delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
-    .then(() => callback());
-
-  return {
-    type: DELETE_POST,
-    payload: id
-  };
-}
+export function createBook(newBook) {
+    return fetch('/books', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newBook) 
+        })
+        .then(response => response.json())
+        .then(book => this.setState({
+          books: this.state.books.concat(book)
+        }))
+        .catch(err => console.log("Error of: ", err))
+        
+        return {
+            type: 'ADD_BOOK',
+            title, 
+            description, 
+            image_url
+        };
+  }
