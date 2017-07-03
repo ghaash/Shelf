edab995be926102345166f41fdd4f41d9531eddf
ghaash <daniel.silverman@me.com>
2017-07-03 10:52:10 -0700

    adding actions

--- client/src/components/Actions/BookAction.js
+++ client/src/components/Actions/BookAction.js
@@ -11,3 +11,20 @@ const actions = {
         };
     }
 };
+
+export const CREATE_POST = "create_post";
+
+export function createBook(newBook) {
+    return fetch('/books', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newBook) 
+        })
+        .then(response => response.json())
+        .then(book => this.setState({
+          books: this.state.books.concat(book)
+        }))
+        .catch(err => console.log("Error of: ", err))
+  }
