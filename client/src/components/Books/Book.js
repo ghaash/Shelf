0da13c476f2040f1893409a62eb298e2d5f1824e
ghaash <daniel.silverman@me.com>
2017-07-15 19:00:44 -0700

    attempted to add vote in stateful book, failed

--- client/src/components/Books/Book.js
+++ client/src/components/Books/Book.js
@@ -1,11 +1,99 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Book = ({ id, title, image_url, description, votes }) => (
-  <div key={id} className='book-div'>
-	<h2>Title: {title} </h2>
-    <img src={image_url} alt={title} />
-    <p>Description: {description}</p>
-    <h1>{this.counter}</h1>
+export default class Book extends Component {
+  
+  constructor() {
+    super() 
+
+    this.state = {
+      books: [],
+      counter: 0
+    } 
+      this.createBook = this.createBook.bind(this)
+      this.handleOnUpVote = this.handleOnUpVote.bind(this)
+      this.handleOnDownVote = this.handleOnDownVote.bind(this)
+    
+  }
+
+ handleOnUpVote(event) {
+    console.log("I'm getting clicked")
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+
+  handleOnDownVote(event) {
+    console.log("I'm getting clicked")
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  }
+
+  createBook(newBook) {
+    return fetch('/books', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newBook) 
+        })
+        .then(response => response.json())
+        .then(book => this.setState({
+          books: this.state.books.concat(book)
+        }))
+        .catch(err => console.log("Error of: ", err))
+  }
+
+  componentDidMount() {
+    return fetch('/books', {
+      method: 'GET', 
+      headers: {
+        'Accepts': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(response => response.json())
+    .then(books => this.setState({ books }))
+    .catch(err => console.log("error is: ", err))
+  }
+
+// render () {
+//   return(
+//      <div key={id} className='book-div'>
+// 	<h2>Title: {title} </h2>
+//     <img src={image_url} alt={title} />
+//     <p>Description: {description}</p>
+//     <h1>{this.state.counter}</h1>
+//             <button 
+//                 onClick={this.handleOnDownVote} 
+//                 className="Downvote">
+//                 Downvote
+//              </button>
+//              <button 
+//                 onClick={this.handleOnUpVote} 
+//                 className="Upvote">
+//                 Upvote
+//              </button>
+//   </div>
+//   )
+// }
+
+
+  render() {
+    console.log(this.state.books);
+    return (
+      <div className="App">
+        {this.state.books.map((book) => {
+          return (
+            <div 
+              key={book.id}
+              className="book-div">
+              <h1>Title: {book.title}</h1>
+              <img 
+                src={book.image_url}
+                alt={book.title} />
+              <p>Description: {book.description}</p>
+              <h1>{this.state.counter}</h1>
             <button 
                 onClick={this.handleOnDownVote} 
                 className="Downvote">
@@ -16,7 +104,32 @@ const Book = ({ id, title, image_url, description, votes }) => (
                 className="Upvote">
                 Upvote
              </button>
-  </div>
-) 
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
 
-export default Book;
+}
+
+// const Book = ({ id, title, image_url, description, votes }) => (
+//   <div key={id} className='book-div'>
+// 	<h2>Title: {title} </h2>
+//     <img src={image_url} alt={title} />
+//     <p>Description: {description}</p>
+//     <h1>{this.state.counter}</h1>
+//             <button 
+//                 onClick={this.handleOnDownVote} 
+//                 className="Downvote">
+//                 Downvote
+//              </button>
+//              <button 
+//                 onClick={this.handleOnUpVote} 
+//                 className="Upvote">
+//                 Upvote
+//              </button>
+//   </div>
+// ) 
+// 
+// export default Book;
--- client/src/components/Books/Books.js
+++ client/src/components/Books/Books.js
@@ -9,13 +9,12 @@ export default class Books extends Component {
     super() 
 
     this.state = {
-      books: [],
-      counter: 0
+      // books: [],
+      // counter: 0
     }
 
-    this.createBook = this.createBook.bind(this)
-    this.handleOnUpVote = this.handleOnUpVote.bind(this)
-    this.handleOnDownVote = this.handleOnDownVote.bind(this)
+    // this.createBook = this.createBook.bind(this)
+
   }
 
   // handleOnUpVote(bookId) {
@@ -29,55 +28,58 @@ export default class Books extends Component {
   //   });
   // }
 
-  handleOnUpVote(event) {
-    this.setState({
-      counter: counter + 1,
-    });
-  }
+  // handleOnUpVote(event) {
+  //   console.log("I'm getting clicked")
+  //   this.setState({
+  //     counter: this.state.counter + 1,
+  //   });
+  // }
 
 
 
-  handleOnDownVote(bookId, event) {
-    console.log("Hello, I'm getting logged")
-    //on click this button increments by one
-     this.setState({
-      counter: this.state.books.counter - 1
-    });
-  }
+  // handleOnDownVote(bookId, event) {
+  //   console.log("Hello, I'm getting logged")
+  //   //on click this button increments by one
+  //    this.setState({
+  //     counter: this.state.books.counter - 1
+  //   });
+  // }
 
-  createBook(newBook) {
-    return fetch('/books', {
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newBook) 
-        })
-        .then(response => response.json())
-        .then(book => this.setState({
-          books: this.state.books.concat(book)
-        }))
-        .catch(err => console.log("Error of: ", err))
-  }
+  // createBook(newBook) {
+  //   return fetch('/books', {
+  //           method: 'POST', 
+  //           headers: {
+  //               'Content-Type': 'application/json'
+  //           },
+  //           body: JSON.stringify(newBook) 
+  //       })
+  //       .then(response => response.json())
+  //       .then(book => this.setState({
+  //         books: this.state.books.concat(book)
+  //       }))
+  //       .catch(err => console.log("Error of: ", err))
+  // }
 
-  componentDidMount() {
-    return fetch('/books', {
-      method: 'GET', 
-      headers: {
-        'Accepts': 'application/json',
-        'Content-Type': 'application/json'
-      },
-    })
-    .then(response => response.json())
-    .then(books => this.setState({ books }))
-    .catch(err => console.log("error is: ", err))
-  }
+  // componentDidMount() {
+  //   return fetch('/books', {
+  //     method: 'GET', 
+  //     headers: {
+  //       'Accepts': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     },
+  //   })
+  //   .then(response => response.json())
+  //   .then(books => this.setState({ books }))
+  //   .catch(err => console.log("error is: ", err))
+  // }
 
   render() {
     console.log(this.state.books);
     return (
       <div className="Books">
-        {this.state.books.map((book) => {
+        <Book 
+        />
+        {/*{this.state.books.map((book) => {
           return (
           <div >
             <Book 
@@ -85,8 +87,6 @@ export default class Books extends Component {
               title={book.title} 
               image_url={book.image_url} 
               description={book.description} 
-              votes={book.votes}
-              onVote={this.handleBookUpVote}
             />
             {/*<h1>{book.counter}</h1>
             <button 
@@ -100,9 +100,9 @@ export default class Books extends Component {
                 Upvote
              </button>*/}
             </div>
-          )
-        })} 
-      </div>
+      //     )
+      //   })} 
+      // </div>*/}
     ); 
   }
 }
