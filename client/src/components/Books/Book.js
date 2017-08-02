4d93481fb563f191f3e5c024daa16ed95e541491
ghaash <daniel.silverman@me.com>
2017-08-02 14:36:10 -0700

    connected api to front end

--- client/src/components/Books/Book.js
+++ client/src/components/Books/Book.js
@@ -5,28 +5,7 @@ export default class Book extends Component {
     console.log(props)
     super(props)
     this.state = props
-    // this.state.id = props.id
-    // this.state.title = props.title
-    // this.state.votes = props.votes
-    // this.state.image_url = props.image_url
-    // this.state.description = props.description
-
-    // this.handleUpvote = this.handleUpvote.bind(this)
   }
-
-  // handleUpvote(){
-  //   // // actually fire AJAX to update the server
-  //   // PATCH /books/1?vote=up
-  //   // PATCH
-  //   // // debugger
-  //   // // this.state.votes = this.state.votes + 1
-  //   // // this.setState({votes: this.state.votes})
-  //   // // // Correct
-  //   this.setState((prevState, props) => ({
-  //     votes: prevState.votes + 1
-  //   }));
-  // }
-
 
   handleUpvote = () => {
     const dataObj = {votes: this.state.votes + 1}
@@ -43,18 +22,21 @@ export default class Book extends Component {
     .then(book => this.setState({votes: book.votes}))
   } 
 
-  // componentDidMount() {
-  //   return fetch('/books', {
-  //     method: 'GET',  jkhl
-  //     headers: {
-  //       'Accepts': 'application/json',
-  //       'Content-Type': 'application/json'
-  //     },
-  //   })
-  //   .then(response => response.json())
-  //   .then(books => this.setState({ books }))
-  //   .catch(err => console.log("error is: ", err))
-  // }
+  handleDownvote = () => {
+    const dataObj = {votes: this.state.votes - 1}
+    console.log(this)
+    return fetch(`/books/${this.state.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Accepts': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dataObj)
+    })
+    .then(response => response.json())
+    .then(book => this.setState({votes: book.votes}))
+  } 
+
 
   render() {
     return(
@@ -64,30 +46,11 @@ export default class Book extends Component {
         <img src={this.state.image_url} alt={this.state.title} />
         <p>Description: {this.state.description}</p>
         <p>Votes: {this.state.votes}</p>
-        <button className="Downvote">Downvote</button>
+        <button onClick={this.handleDownvote} className="Downvote">Downvote</button>
         <button onClick={this.handleUpvote} className="Upvote">Upvote</button>
       </div>
     )
   }
 }
-
-// handleOnUpVote(event) {
-//     console.log("Hello, I'm getting logged")
-//     //on click this button increments by one
-//      this.setState({
-//       book: this.votes + 1
-//     });
-//   }
-
-//   handleOnDownVote(event) {
-//     console.log("Hello, I'm getting logged")
-//     //on click this button increments by one
-//      this.setState({
-//       book: this.votes - 1
-//     });
-//   }
-
-// this.handleOnUpVote = this.handleOnUpVote.bind(this)
-// this.handleOnDownVote = this.handleOnDownVote.bind(this)
 
    
